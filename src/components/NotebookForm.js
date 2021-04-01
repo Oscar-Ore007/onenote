9e06310ba42b8240f043268ace3c9bdb34e375e9
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-01 11:08:34 -0400

    created NotebookForm component

---
+++ src/components/NotebookForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NotebookFormContainer from '../containers/NotebookFormContainer';
+
+class NotebookForm extends Component {
+    render() {
+        return(
+            <div>
+                Input notebook here! 
+            </div>
+        )
+    }
+}
+
+export default NotebookForm; 
