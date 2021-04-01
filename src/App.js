2c3234bbaf1b9927a6b5c9594fbb8f16042c94be
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-01 10:10:00 -0400

    create Notebooks component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import fetchNotebooks from './actions/fetchNotebooks';
 import NotebooksContainer from './containers/NotebooksContainer';
+import NotebookFormContainer from './containers/NotebookFormContainer';
 
 class App extends Component {
  componentDidMount() {
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <div>
         <NotebooksContainer /> 
+        <NotebookFormContainer /> 
       </div>
     )
   }
---
+++ src/components/Notebook.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Notebooks extends Component {
+    render() {
+        return(
+            <div>
+                List of Notebooks
+            </div>
+        )
+    }
+}
+
+export default Notebooks; 
