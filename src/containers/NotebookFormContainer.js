0dc8e4a1359d58521af9d96311c9daf175e2be58
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-01 11:12:09 -0400

    import NotebookForm into NotebookForm Container

--- src/containers/NotebookFormContainer.js
+++ src/containers/NotebookFormContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import NotebookForm from '../components/NotebookForm';
 
 class NotebookFormContainer extends Component {
     render() {
         return (
             <div>
                 NotebookFormContainer
+                <NotebookForm /> 
             </div>
         )
     }
