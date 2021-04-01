14578a2e6c64f2048691a29f5c37140c0fbddee1
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-01 11:04:50 -0400

    import Notebooks into Notebooks Container

--- src/containers/NotebooksContainer.js
+++ src/containers/NotebooksContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Notebooks from '../components/Notebooks';
 
 class NotebooksContainer extends Component {
     render() {
         return(
             <div>
                 Notebook Container! 
+                <Notebooks /> 
             </div>
         )
     }
