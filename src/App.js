b034b8b5e97de4b5242a6c1c4f51b30949d48907
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-01 02:50:10 -0400

    add componentDidMount to App to fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    fetch('http://localhost:3000/notebooks')
+    .then(response => response.json())
+    .then(data => console.log(data[0].notes))
+  }
+
+  render () {
+    return (
+      <div>
+        Hello World 
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App; 
