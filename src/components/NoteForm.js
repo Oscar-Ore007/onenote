b94fe1f08ee401359f125e9070ce914a152efc87
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-01 22:18:42 -0400

    write handleSubmit function and add onSubmit

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class NoteForm extends Component {
+
+    state = {
+        title: "",
+        content: "",
+        date: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+    }
     render() {
         return (
             <div>
                 Write your notes here:
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Title" name="title" value={this.state.title} onChange={this.handleChange} /><br></br>
+
+                    <textarea type="textarea" placeholder="Start writing" name="content" value={this.state.content} rows={15} cols={100} onChange={this.handlechange}/><br></br>
+                    <input type="submit" /> 
+                </form>
             </div>
         )
     }
 }
 
-export default Noteform 
+export default connect(null)(Noteform) 
