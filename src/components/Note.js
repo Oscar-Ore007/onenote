947aa3a7ce01fbf5e47480b47c2b8ff50c40bbd7
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-01 23:13:47 -0400

    update Note Component to render the date, title and content

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 function Note(props) {
+
+    let note = props.note 
     return (
         <div>
+            {note && note.date }
+            <h3>{note.title}</h3>
+            <p>{note.content}</p>
             Note!!
         </div>
     )
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
 import Note from './Note';
 
 function Notes(props) {
 
     return (
         <div>
+            <br/> 
             All notes will go here:
+            <ul>
             {props.notes && props.notes.map(note =>
-                <li key={note.id}>{note.title}</li>
+                <li key={note.id}>
+                    <Note note={note}/>
+                </li>
                 )}
+                </ul>
         </div>
     )
 }
