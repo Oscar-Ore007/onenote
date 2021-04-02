7982971ff8ff6e045c7babc92519a67cede5fe06
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-01 20:27:12 -0400

    define handle change and added onChange to input

--- src/components/NotebookForm.js
+++ src/components/NotebookForm.js
@@ -4,13 +4,19 @@ import NotebookFormContainer from '../containers/NotebookFormContainer';
 class NotebookForm extends Component {
 
     state = {title: ""}
-    
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value 
+        })
+    }
+
     render() {
         return(
             <div>
                 Input notebook here! 
                 <form>
-                    <input type="text" placeholder="Title" /> 
+                    <input type="text" placeholder="Title" value={this.state.title} name="title" onChange={this.handleChange} /> 
                 </form>
             </div>
         )
