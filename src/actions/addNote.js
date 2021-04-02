269ec4ddfa62dff9b0766ddc1f7b84be2b98235e
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-02 00:59:43 -0400

    updated addNote action

--- src/actions/addNote.js
+++ src/actions/addNote.js
@@ -1,34 +1,20 @@
 export default function addNote(note, notebookId) {
-    // return (dispatch) => {
-    //     const options = {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             "Accept": "application/json"
-    //         },
-
-    //         body: JSON.stringify({note})
-    //     }
-    //     fetch('http://localhost:3000/notebooks/${notebookId}/notes', options)
-    //     .then(response => response.json())
-    //     .then(note => {
-    //         dispatch({type: 'ADD_NOTE', payload: note})
-    //     })
-    // }
-
     return (dispatch) => {
+        // debugger
         const options = {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json"
             },
-            body: JSON.stringify({note})
+            body: JSON.stringify(note)
         }
+        
         fetch(`http://localhost:3000/notebooks/${notebookId}/notes`, options)
         .then(response => response.json())
         .then(note => {
             dispatch({type: 'ADD_NOTE', payload: note})
-    })
+        })
+        
     }
 }
