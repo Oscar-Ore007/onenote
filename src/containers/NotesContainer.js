3fa9bebb4e2a983a235bf87fef6fa8328e23952b
Oscar Ore <oscar.ore2011@gmail.com>
2021-04-01 22:06:46 -0400

    create NoteForm component

--- src/containers/NotesContainer.js
+++ src/containers/NotesContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Notes from '../components/Notes';
+import NoteForm from '../components/NoteForm';
 
 class NotesContainer extends Component {
     render() {
         return (
             <div>
                 My Notes Container 
+                <NoteForm /> 
                 <Notes /> 
             </div>
         )
